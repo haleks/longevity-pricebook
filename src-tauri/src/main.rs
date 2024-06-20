haleksandre/tauri-sveltekit

feat(init): git initialized

src-tauri/src/main.rs
@@ -1,0 +1,6 @@
+// Prevents additional console window on Windows in release, DO NOT REMOVE!!
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+
+fn main() {
+    vndbracket_lib::run()
+}

haleks/longevity-pricebook

src-tauri/src/main.rs
@@ -1,0 +1,6 @@
+// Prevents additional console window on Windows in release, DO NOT REMOVE!!
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+
+fn main() {
+    pricebooklib::run()
+}